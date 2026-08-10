627226e9e3ced75ee8562b154af862e31175da77
RAZAELE <efloresr10@miumg.edu.gt>
2026-08-09 23:16:10 -0600

    Se terminó de crear el formulario visual

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,96 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
-const styles =StyleSheet.create({
+export default function Index() {
+  return (
+    <View style={styles.container}>
+
+      <View style={styles.card}>
+        <Text style={styles.title}>
+          TARJETA DE ESTUDIANTE
+        </Text>
+        <View style={styles.content}>
+          <View style={styles.info}>
+
+            <Text style={styles.label}>
+              Nombre:
+            </Text>
+
+            <Text style={styles.text}>
+              Eleazar Natanael Flores Ramos
+            </Text>
+
+            <Text style={styles.label}>
+              Carné:
+            </Text>
+
+            <Text style={styles.text}>
+              0907-25-19873
+            </Text>
+
+            <Text style={styles.label}>
+              Carrera:
+            </Text>
+
+            <Text style={styles.text}>
+              Ingeniería en Sistemas
+            </Text>
+
+            <Text style={styles.label}>
+              Semestre:
+            </Text>
+
+            <Text style={styles.text}>
+              4to. Semestre
+            </Text>
+
+          </View>
+          <Image
+            source={require('../../assets/images/feo.jpg')}
+            style={styles.photo}
+          />
+
+        </View>
+        <Text style={styles.footer}>
+          Universidad Mariano Gálvez
+        </Text>
+
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
     padding: 20,
   },
 
   card: {
-    width: '90%',
-    padding: 20,
-    margin: 10,
-    borderWidth: 1,
+    width: '100%',
+    padding: 30,
     borderRadius: 10,
+    backgroundColor: '#93efff',
   },
 
   title: {
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'center',
-    marginBottom: 20,
+    marginBottom: 30,
+  },
+
+  content: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 
   info: {
-    flexDirection: 'column',
-    marginBottom: 20,
+    flex: 1,
+    marginRight: 30,
   },
 
   label: {
@@ -34,11 +99,18 @@ const styles =StyleSheet.create({
   },
 
   text: {
-    marginTop: 3,
+    marginTop: 5,
+  },
+
+  photo: {
+    width: 180,
+    height: 180,
+    borderRadius: 90,
   },
 
   footer: {
     textAlign: 'center',
-    marginTop: 10,
+    marginTop: 35,
   },
-});
+
+})
